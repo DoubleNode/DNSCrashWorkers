d8c960dcee2f860be2303c50b35cb42b14fb7ebf
Darren Ehlers <me@darrenehlers.com>
2021-03-19 10:43:40 -0500

    Updated Validation protocol

--- Sources/DNSCrashWorkers/WKRCrashValidationWorker.swift
+++ Sources/DNSCrashWorkers/WKRCrashValidationWorker.swift
@@ -8,119 +8,98 @@
 
 import DNSBlankWorkers
 import DNSCore
+import DNSError
 import DNSProtocols
 import Foundation
 
 open class WKRCrashValidationWorker: WKRBlankValidationWorker
 {
     // MARK: - Business Logic / Single Item CRUD
-    override open func doValidateBirthdate(for birthdate: Date,
-                                           with progress: PTCLProgressBlock?,
-                                           and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateBirthdate(for birthdate: Date) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateBirthdate(for: birthdate, with: progress, and: block)
+        return try nextWorker!.doValidateBirthdate(for: birthdate)
     }
 
-    override open func doValidateEmail(for email: String,
-                                       with progress: PTCLProgressBlock?,
-                                       and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateEmail(for email: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateEmail(for: email, with: progress, and: block)
+        return try nextWorker!.doValidateEmail(for: email)
     }
 
-    override open func doValidateHandle(for handle: String,
-                                        with progress: PTCLProgressBlock?,
-                                        and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateHandle(for handle: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateHandle(for: handle, with: progress, and: block)
+        return try nextWorker!.doValidateHandle(for: handle)
     }
 
-    override open func doValidateName(for name: String,
-                                      with progress: PTCLProgressBlock?,
-                                      and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateName(for name: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateName(for: name, with: progress, and: block)
+        return try nextWorker!.doValidateName(for: name)
     }
     
-    override open func doValidateNumber(for number: String,
-                                        with progress: PTCLProgressBlock?,
-                                        and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateNumber(for number: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateNumber(for: number, with: progress, and: block)
+        return try nextWorker!.doValidateNumber(for: number)
     }
                                       
-    override open func doValidatePassword(for password: String,
-                                          with progress: PTCLProgressBlock?,
-                                          and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidatePassword(for password: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidatePassword(for: password, with: progress, and: block)
+        return try nextWorker!.doValidatePassword(for: password)
     }
 
-    override open func doValidatePercentage(for percentage: String,
-                                            with progress: PTCLProgressBlock?,
-                                            and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidatePercentage(for percentage: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidatePercentage(for: percentage, with: progress, and: block)
+        return try nextWorker!.doValidatePercentage(for: percentage)
     }
 
-    override open func doValidatePhone(for phone: String,
-                                       with progress: PTCLProgressBlock?,
-                                       and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidatePhone(for phone: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidatePhone(for: phone, with: progress, and: block)
+        return try nextWorker!.doValidatePhone(for: phone)
     }
 
-    override open func doValidateSearch(for search: String,
-                                        with progress: PTCLProgressBlock?,
-                                        and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateSearch(for search: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateSearch(for: search, with: progress, and: block)
+        return try nextWorker!.doValidateSearch(for: search)
     }
 
-    override open func doValidateState(for state: String,
-                                       with progress: PTCLProgressBlock?,
-                                       and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateState(for state: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateState(for: state, with: progress, and: block)
+        return try nextWorker!.doValidateState(for: state)
     }
 
-    override open func doValidateUnsignedNumber(for number: String,
-                                                with progress: PTCLProgressBlock?,
-                                                and block: PTCLValidationBlockVoidBoolDNSError?) throws {
+    override open func doValidateUnsignedNumber(for number: String) throws -> DNSError? {
         guard nextWorker != nil else {
             throw PTCLBaseError
                 .notImplemented(DNSCrashWorkersCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        try nextWorker!.doValidateUnsignedNumber(for: number, with: progress, and: block)
+        return try nextWorker!.doValidateUnsignedNumber(for: number)
     }
 }
