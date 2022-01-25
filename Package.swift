b318a957823ed850483ceb2cc1cc21d07df5df6b
Darren Ehlers <me@darrenehlers.com>
2022-01-24 21:27:30 -0600

    Renamed SystemsState to Systems protocol

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.7.1"),
+        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.7.2"),
         .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.6.36"),
-        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.6.2"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.7.0"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.6.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.7.5"),
+        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.7.6"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/DNSCrashWorkers/WKRCrashSystemsWorker.swift
+++ Sources/DNSCrashWorkers/WKRCrashSystemsWorker.swift
@@ -1,5 +1,5 @@
 //
-//  WKRCrashSystemsStateWorker.swift
+//  WKRCrashSystemsWorker.swift
 //  DoubleNode Swift Framework (DNSFramework) - DNSCrashWorkers
 //
 //  Created by Darren Ehlers.
@@ -13,22 +13,35 @@ import DNSError
 import DNSProtocols
 import UIKit
 
-open class WKRCrashSystemsStateWorker: WKRBlankSystemsStateWorker
+open class WKRCrashSystemsWorker: WKRBlankSystemsWorker
 {
     @available(*, unavailable, message: "Unable to chain CrashWorker(s)")
     public required init(call callNextWhen: PTCLProtocol.Call.NextWhen,
-                         nextWorker: PTCLSystemsState) { fatalError("Unable to chain CrashWorker(s)") }
+                         nextWorker: PTCLSystems) { fatalError("Unable to chain CrashWorker(s)") }
     
     public required init() { super.init() }
     
     // MARK: - Internal Work Methods
+    override open func intDoLoadSystem(for id: String,
+                                       with progress: PTCLProgressBlock?,
+                                       and block: PTCLSystemsBlockVoidSystem?,
+                                       then resultBlock: PTCLResultBlock?) throws {
+        throw DNSError.Systems
+            .notImplemented(DNSCodeLocation.crashWorkers(self, "\(#file),\(#line),\(#function)"))
+    }
+    override open func intDoLoadSystems(with progress: PTCLProgressBlock?,
+                                        and block: PTCLSystemsBlockVoidArraySystem?,
+                                        then resultBlock: PTCLResultBlock?) throws {
+        throw DNSError.Systems
+            .notImplemented(DNSCodeLocation.crashWorkers(self, "\(#file),\(#line),\(#function)"))
+    }
     override open func intDoReportState(of state: String,
                                         for system: String,
                                         and endPoint: String,
                                         with progress: PTCLProgressBlock?,
                                         then resultBlock: PTCLResultBlock?) -> AnyPublisher<Bool, Error> {
         return Future<Bool, Error> {
-            let error = DNSError.SystemsState
+            let error = DNSError.Systems
                 .notImplemented(DNSCodeLocation.crashWorkers(self, "\(#file),\(#line),\(#function)"))
             $0(.failure(error))
         }.eraseToAnyPublisher()
