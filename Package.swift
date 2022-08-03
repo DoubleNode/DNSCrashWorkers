67bd280de3d983203a9254b3d0320b26705b7675
Darren Ehlers <me@darrenehlers.com>
2022-08-03 12:01:36 -0500

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.9.15"),
+        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.9.24"),
         .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.9.13"),
         .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.9.21"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.8.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.9.39"),
+        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.9.53"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
