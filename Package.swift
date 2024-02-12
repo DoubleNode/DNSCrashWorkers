4109cb8dd687130b42eab1b2f094c494a4c00d9b
Darren Ehlers <me@darrenehlers.com>
2024-02-12 16:16:27 -0600

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -26,11 +26,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.11.11"),
+        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.11.12"),
         .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.11.3"),
-        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.11.2"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.11.3"),
         .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.11.0"),
-        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.11.3"),
+        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.11.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- README.md
+++ README.md
@@ -34,17 +34,17 @@ Once you have your Swift package set up, adding Alamofire as a dependency is as 
 
 ```swift
 dependencies: [
-.package(url: "https://github.com/DoubleNode/DNSCrashWorkers.git", from: "1.11.11")
+.package(url: "https://github.com/DoubleNode/DNSCrashWorkers.git", from: "1.11.12")
 ]
 ```
 
 ## Dependencies
 
--   DNSBlankWorkers v1.11.11+
+-   DNSBlankWorkers v1.11.12+
 -   DNSCore v1.11.3+
--   DNSDataObjects v1.11.2+
+-   DNSDataObjects v1.11.3+
 -   DNSError v1.11.0+
--   DNSProtocols v1.11.3+
+-   DNSProtocols v1.11.4+
 
 ## Credits
 
